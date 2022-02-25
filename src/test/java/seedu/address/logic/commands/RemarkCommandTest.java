1b87bf24104e0a062ef28610bce3e68afda7672d
tiewweijian <tiewweijian@u.nus.edu>
2022-02-25 23:18:29 +0800

    Rectify checkstyle violations

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -1,6 +1,16 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.model.AddressBook;
@@ -11,25 +21,20 @@ import seedu.address.model.person.Person;
 import seedu.address.model.person.Remark;
 import seedu.address.testutil.PersonBuilder;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static seedu.address.logic.commands.CommandTestUtil.*;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 class RemarkCommandTest {
 
     private static final String REMARK_STUB = "Some remark";
-    public static final String VALID_NAME_AMY = "Amy Bee";
-    public static final String VALID_NAME_BOB = "Bob Choo";
-    public static final String VALID_PHONE_AMY = "11111111";
-    public static final String VALID_PHONE_BOB = "22222222";
-    public static final String VALID_EMAIL_AMY = "amy@example.com";
-    public static final String VALID_EMAIL_BOB = "bob@example.com";
-    public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
-    public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
-    public static final String VALID_TAG_HUSBAND = "husband";
-    public static final String VALID_TAG_FRIEND = "friend";
+    private static final String VALID_NAME_AMY = "Amy Bee";
+    private static final String VALID_NAME_BOB = "Bob Choo";
+    private static final String VALID_PHONE_AMY = "11111111";
+    private static final String VALID_PHONE_BOB = "22222222";
+    private static final String VALID_EMAIL_AMY = "amy@example.com";
+    private static final String VALID_EMAIL_BOB = "bob@example.com";
+    private static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
+    private static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
+    private static final String VALID_TAG_HUSBAND = "husband";
+    private static final String VALID_TAG_FRIEND = "friend";
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
