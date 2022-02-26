ec9307f6d16f272b509c5f010aaa6e4af7ebe2f7
leeenen <leeenen@outlook.com>
2022-02-26 11:17:16 +0800

    Rearrange imports to satisfy checkstyle

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Creates a remark object.
+     *
+     * @param remark A remark to be added.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
