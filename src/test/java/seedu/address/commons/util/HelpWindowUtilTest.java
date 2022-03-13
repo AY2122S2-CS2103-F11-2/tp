61274051cba7c1e7f62df9b3011223fa2d3089cc
tiewweijian <tiewweijian@u.nus.edu>
2022-03-13 14:18:03 +0800

    Add new test cases for invalid links

--- src/test/java/seedu/address/commons/util/HelpWindowUtilTest.java
+++ src/test/java/seedu/address/commons/util/HelpWindowUtilTest.java
@@ -1,6 +1,7 @@
 package seedu.address.commons.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
 import java.net.URI;
@@ -10,9 +11,16 @@ import org.junit.jupiter.api.Test;
 class HelpWindowUtilTest {
 
     private static String validLink = "https://github.com/AY2122S2-CS2103-F11-2";
+    private static String invalidLink = "invalid link";
 
     @Test
     public void userGuide_checkLink_throwsIoException() throws IOException {
         assertEquals(HelpWindowUtil.convertToUrl(validLink), URI.create("https://github.com/AY2122S2-CS2103-F11-2"));
     }
+
+    @Test
+    public void userGuide_checkInvalidLink_throwsIoException() throws IOException {
+        assertThrows(IllegalArgumentException.class, () -> HelpWindowUtil.convertToUrl(invalidLink));
+    }
+
 }
